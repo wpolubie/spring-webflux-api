dcb699c2a0221d6cf2813b8e90e71244526d7fff
Wojan93 <wojciechpolubiec@gmail.com>
2021-04-12 14:56:55 +0200

    all values from database directed to the REST controller

--- src/main/java/com/example/springwebfluxapi/StudentApi.java
+++ src/main/java/com/example/springwebfluxapi/StudentApi.java
@@ -14,6 +14,6 @@ public class StudentApi {
     }
     @GetMapping
     public Flux<String> getAll() {
-        return Flux.just("Anna", "Andrzej", "Barbara", "Cezary", "Karol", "Karolina", "Krzysztof", "Wojciech", "Zbigniew", "Zuzanna");
+        studentRepo.findAll();
     }
 }
